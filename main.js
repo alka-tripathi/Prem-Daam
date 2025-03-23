ae8b9c06c701051babddeb1a309ec2f32883ae7a
Alka Tripathi <tripathialka382@gmail.com>
2025-03-23 23:49:38 +0530

    home page

--- main.js
+++ main.js
@@ -1,18 +1,37 @@
+import { initializeApp } from 'firebase/app';
+
+// TODO: Replace the following with your app's Firebase project configuration
+
+
 
 
   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-analytics.js";
+  import { getAuth,GoogleAuthProvider,signInWithPopup } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js";
+  // import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-analytics.js";
   
-  const firebaseConfig = {
-    apiKey: "AIzaSyB8U5tIwItwS6-q5GW3We7OMkPz-Xz463Y",
-    authDomain: "goolge-auth-69e2f.firebaseapp.com",
-    projectId: "goolge-auth-69e2f",
-    storageBucket: "goolge-auth-69e2f.firebasestorage.app",
-    messagingSenderId: "976983603186",
-    appId: "1:976983603186:web:4a4390ce83662c9da39d6b",
-    measurementId: "G-06HR53V1Q5"
-  };
+  
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
+
+  const auth=getAuth(app);
+  auth.languageCode='en';
+  const provider = new GoogleAuthProvider();
+
+  const googleLogin=document.getElementById("google-login-btn");
+  googleLogin.addEventListener("click",function(){
+   signInWithPopup(auth,provider)
+   .then((result)=>{
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const user = result.user;
+    console.log(user);
+    window.location.href="../logged.html";
+   })
+   .catch((error)=>{
+    const errorCode = error.code;
+    const errorMess=error.message;
+
+   })
+  })
+
